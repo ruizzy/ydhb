47667d4ae5a1ff6214067785e8e7054341f4b501
KaiZhang <1241548041@qq.com>
2017-12-18 17:34:31 +0800

    修改查询结果页面

--- src/components/car_undwrt/module/CarUWSearchResult.vue
+++ src/components/car_undwrt/module/CarUWSearchResult.vue
@@ -1,33 +1,29 @@
 <template>
-  <div id="car-uw-search-result" class="vux-form-preview weui-form-preview">
-    <scroller lock-x use-pullup :pullup-config="pullopt" @on-pullup-loading="loadMore" ref="scroller" :height="height">
-      <div class="box2">
-        <div class="weui-form-preview__bd" v-for="item in list" @click="goTaskHandle(item)">
-          <div class="weui-form-preview__item">
-            <label class="weui-form-preview__label">{{$t(labels.text_a)}}</label>
-            <span class="weui-form-preview__value">{{$t(item.businessNo)}}</span>
-          </div>
-          <div class="weui-form-preview__item">
-            <label class="weui-form-preview__label">{{$t(labels.text_b)}}</label>
-            <span class="weui-form-preview__value">{{$t(item.appliName)}}</span>
-          </div>
-          <div class="weui-form-preview__item">
-            <label class="weui-form-preview__label">{{$t(labels.text_c)}}</label>
-            <span class="weui-form-preview__value">{{$t(item.firstTrial)}}</span>
-          </div>
-        </div>
-      </div>
+  <div id="car-uw-search-result">
+    <scroller lock-x
+              scrollbarY
+              use-pullup :pullup-config="pullopt" @on-pullup-loading="loadMore" ref="scroller">
       <div v-show="noData" class="xs-plugin-pullup-container xs-plugin-pullup-undefined xs-plugin-pullup-up gl-nodata">查询不到更多的记录</div>
     </scroller>
   </div>
 </template>
 
 <script>
-  import { Scroller, Divider, XSwitch, Group, Spinner } from 'vux'
-  import {taskQuery, taskHandle} from 'business'
+  import {
+    Scroller,
+    Group,
+    Cell } from 'vux'
+  import {
+    taskQuery,
+    taskHandle } from 'business'
   import carService from '../carService'
   export default {
     name: 'car-uw-search-result',
+    components: {
+      Scroller,
+      Group,
+      Cell
+    },
     data () {
       return {
         noData: false,
@@ -101,27 +97,9 @@
       }
     },
     computed: {
-    },
-    components: {
-      Scroller,
-      Divider,
-      XSwitch,
-      Group,
-      Spinner
-    },
+    }
   }
 </script>
 
 <style lang="less">
-@import '../../../styles/weui/widget/weui_cell/weui_form/weui-form-preview.less';
-  #car-uw-search-result {
-    .weui-form-preview__bd {
-      border-bottom: 1px solid #D6D6D6;
-    }
-  }
-  #app {
-    .weui-form-preview__bd {
-      border-bottom: 1px solid #D6D6D6;
-    }
-  }
 </style>
